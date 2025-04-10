26691dd8f60c8387c37386a64aeabd055868bab4
IreneVarisco <variscoirene@gmail.com>
2025-04-10 12:54:24 +0200

    nodejs examples

--- Lezione 8/film.js
+++ Lezione 8/film.js
@@ -1,7 +1,7 @@
-//console.log("Hello World"); 
+console.log("Hello World"); 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3080;
 
 app.get("/", (req, res) => {
   console.log("richiesta");
@@ -34,7 +34,7 @@ app.get("/user/:id", (req, res) => {
   if (id <= 30 && id >= 0) {
     res.json(risposta);
     //console.log("richiesta utente con id: " + id);
-    //res.send(`utente 1: ${id} <br> lingua: ${lang} <br> q: ${q} <br> page: ${page}`);
+    //res.send(utente 1: ${id} <br> lingua: ${lang} <br> q: ${q} <br> page: ${page});
   } else {
     res.status(404).send("utente non trovato")
   }
@@ -50,7 +50,19 @@ app.get("/user/:id/film/:film", (req, res) => {
   const id = req.params.id;
   const film = req.params.film;
 
-  res.send(`film preferito: ${id} <br> film preferito: ${film}`);
+  const risp={
+    identificatore: parseInt(id),
+    id: parseInt(film),
+    title: "LA PAURA",
+    genere: ["horror", "drammatico"],
+    release_date: "2023-11-22",
+    runtime: "1m 41s",
+    overview: "Un giorno nella vita di una donna tormentata dalla sua Paura",
+    id_regista: 869879,
+    tipo: "cortometraggio"
+  }
+
+    res.json(risp);
 })
 
 app.listen(port, () => {
